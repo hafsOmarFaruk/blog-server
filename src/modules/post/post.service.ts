5f689b2e15456c8508ef9558ba2bbfaeccc97aba
practiceTaqwa <muhammadabuhafz@gmail.com>
2026-06-29 20:25:37 +0600

    Implement pagination and sorting in getAllPosts function

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -3,6 +3,7 @@ import { CommentStatus, PostStatus } from "../../../generated/prisma/enums";
 import { prisma } from "../../lib/prisma";
 import { IcreatePostPayload } from "./post.interface";
 import { PostWhereInput } from "../../../generated/prisma/models";
+import { skip } from "node:test";
 
 const createPost = async (payload: IcreatePostPayload, userId: string) => {
   const result = await prisma.post.create({
@@ -28,6 +29,13 @@ interface IPostQuery extends PostWhereInput {
 }
 
 const getAllPosts = async (query: IPostQuery) => {
+  const limit = query.limit ? Number(query.limit) : 10;
+  const page = query.page ? Number(query.page) : 1;
+  const skip = (page - 1) * limit;
+
+  const sortBy = query.sortBy ? query.sortBy : "createdAt";
+  const sortOrder = query.sortOrder ? query.sortOrder : "desc";
+
   const posts = await prisma.post.findMany({
     where: {
       AND: [
@@ -62,6 +70,13 @@ const getAllPosts = async (query: IPostQuery) => {
       ],
     },
 
+    //pagination
+    take: limit,
+    skip: skip,
+    //sorting
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
     include: {
       author: {
         omit: {
